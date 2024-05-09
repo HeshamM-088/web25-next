cb829d3c0747e38b4de5a916591e78a474dc55b6
HeshamM-088 <heshamkhalil1988@gmail.com>
2024-05-10 01:00:13 +0300

    third

--- app/users/[userId]/page.jsx
+++ app/users/[userId]/page.jsx
@@ -1,4 +1,6 @@
 import axios from "axios";
+import Image from "next/image";
+import TestImage from "@/public/Login-1.png";
 
 const getUser = async (id) => {
   const data = await axios({
@@ -23,20 +25,30 @@ const getDogs = async () => {
     method: "get",
     url: `https://dog.ceo/api/breeds/image/random`,
   });
-
   return data.data;
 };
 
 const UserInfo = async ({ params }) => {
   const { userId } = params;
 
-  const user = await getUser(userId);
-  const posts = await getPosts();
-  const dogs = await getDogs();
+  const [user, post, dog] = await Promise.allSettled([
+    getUser(userId),
+    getPosts(),
+    getDogs(),
+  ]);
 
   return (
     <div className="text-center">
       <h1>UserInfo</h1>
+      {user.value.name}
+      <Image
+        src={`${dog.value.message}`}
+        width={300}
+        height={300}
+        alt="dog logo"
+        className="w-[50%]"
+        priority={true}
+      />
     </div>
   );
 };
